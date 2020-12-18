31379a8e9d5efbe56103703c5e44d133b768e121
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-12-18 17:23:57 +0000

    Update olap.sql

--- olap.sql
+++ olap.sql
@@ -1,3 +1,4 @@
-SELECT severity, locality, week_day, COUNT(*)
+SELECT COALESCE(severity, 'All Severities') as severity, COALESCE(locality, 'All Locations') as locality, COALESCE(week_day, 'All Days') as week_day, COUNT(*)
 FROM f_incident NATURAL JOIN d_location NATURAL JOIN d_time
-GROUP BY CUBE(severity, locality,week_day);
+GROUP BY CUBE(severity, locality,week_day)
+ORDER BY (severity, locality, week_day) DESC;
