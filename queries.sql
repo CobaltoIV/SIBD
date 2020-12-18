70a4571abd44c0d6766a31b98fe43c051cacb82d
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-12-18 17:31:09 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 -- 3 - View   --
-CREATE VIEW supp(sname,saddress,num_subs)
+CREATE VIEW supp(sname, saddress, num_subs)
 AS
-SELECT sname,saddress, COUNT(*) AS num_subs
-FROM  substation
+SELECT sname, saddress, COUNT(*) AS num_subs
+FROM substation
 GROUP BY sname, saddress;
 
 -- 4 - SQL Queries --
@@ -11,37 +11,41 @@ GROUP BY sname, saddress;
 -- Analysts that have analysed every incident of element B-789
 select a.name, a.address
 from analyst a
-where not exists(select id, instant  -- Every incidents on 'B-789'
+where not exists(select id,
+                        instant -- Every incidents on 'B-789'
                  from incident
                  where id = 'B-789'
                      except
-                 select id, instant -- every incident analysed by the analyst in question
+                 select id,
+                        instant -- every incident analysed by the analyst in question
                  from analyses an
-                 where an.name = a.name and an.address = a.address)
+                 where an.name = a.name
+                   and an.address = a.address)
 ;
 
 -- 2 --
 
 SELECT DISTINCT sname, saddress
 FROM substation
-WHERE (sname,saddress) NOT IN(
-    SELECT sname, saddress -- supervisors from substations south of Rio Maior
+WHERE (sname, saddress) NOT IN (
+    SELECT sname,
+           saddress -- supervisors from substations south of Rio Maior
     FROM substation
     WHERE gpslat <= 39.336775);
 
 -- 3 --
 -- Elements with the smallest amount of incidents (element must have atleast one incident to be elligible)
-SELECT id , COUNT(id)
-FROM incident  -- ids come from the incident substation (we don't want elements with 0 incidents)
+SELECT id, COUNT(id)
+FROM incident -- ids come from the incident substation (we don't want elements with 0 incidents)
 GROUP BY id
-HAVING COUNT(id) <= ALL( -- Elements that verify this condition have the smallest amount of incidents
-        SELECT COUNT(i2.id)
-        FROM  incident i2 
-        GROUP BY i2.id);
+HAVING COUNT(id) <= ALL ( -- Elements that less or equal number of incidents than the rest of the elements
+    SELECT COUNT(i2.id)
+    FROM incident i2
+    GROUP BY i2.id);
 
 -- 4 --
 -- How many substations does each supervisor supervise
-SELECT name,address, COALESCE(num_subs,0)
+SELECT name, address, COALESCE(num_subs, 0)
 FROM supervisor s -- Needs base table due to the supervisors with no substations
-        LEFT OUTER JOIN supp ON s.name = supp.sname AND s.address = supp.saddress; -- Use the view created to get all the counts
+         LEFT OUTER JOIN supp ON s.name = supp.sname AND s.address = supp.saddress; -- Use the view created to get all the counts
 
